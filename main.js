e632600adfde5e7cf62e27175c7a96c85832161c
HossainImran <3333mrkhan3333@gmail.com>
2023-07-08 16:22:02 +0600

    Starter and header section completed

---
+++ main.js
@@ -0,0 +1,10 @@
+import './styles/about.css';
+import './styles/contact.css';
+import './styles/footer.css';
+import './styles/header.css';
+import './styles/hero.css';
+import './styles/modern-normalize.css';
+import './styles/projects.css';
+import './styles/skills.css';
+import './styles/style.css';
+
