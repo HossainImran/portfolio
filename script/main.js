935ab18a46ce2be094b1d37a0abed2b6a6ee4537
HossainImran <3333mrkhan3333@gmail.com>
2023-07-10 21:05:44 +0600

    contact and footer section completed

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
-import './styles/about.css';
-import './styles/contact.css';
-import './styles/footer.css';
-import './styles/header.css';
-import './styles/hero.css';
-import './styles/modern-normalize.css';
-import './styles/projects.css';
-import './styles/style.css';
-import './styles/work.css';
+import '../styles/about.css';
+import '../styles/contact.css';
+import '../styles/footer.css';
+import '../styles/header.css';
+import '../styles/hero.css';
+import '../styles/modern-normalize.css';
+import '../styles/projects.css';
+import '../styles/style.css';
+import '../styles/work.css';
 
