f64f87822380f79205e50289794eed318a7b0c98
HossainImran <3333mrkhan3333@gmail.com>
2023-07-12 22:58:47 +0600

    completed

--- script/mobile-nav.js
+++ script/mobile-nav.js
@@ -10,3 +10,33 @@ toggleBtn2.addEventListener('click', () => {
     document.querySelector('.dropdown__nav').style.display = 'none'
     document.querySelector('.toggle__btn2').style.display = 'none'
 })
+
+const dropDownLinks1 = document.querySelector('.js-links1')
+const dropDownLinks2 = document.querySelector('.js-links2')
+const dropDownLinks3 = document.querySelector('.js-links3')
+const dropDownLinks4 = document.querySelector('.js-links4')
+const dropDownLinks5 = document.querySelector('.js-links5')
+    function dropdownOff() {
+        document.querySelector('.dropdown__nav').style.display = 'none'
+        document.querySelector('.toggle__btn2').style.display = 'none'
+    }
+
+dropDownLinks1.addEventListener('click', () => {
+    dropdownOff() 
+})
+dropDownLinks2.addEventListener('click', () => {
+    dropdownOff() 
+})
+
+dropDownLinks3.addEventListener('click', () => {
+    dropdownOff() 
+})
+
+dropDownLinks4.addEventListener('click', () => {
+    dropdownOff() 
+})
+
+dropDownLinks5.addEventListener('click', () => {
+    dropdownOff() 
+})
+
